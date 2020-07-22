643d59706ead26ae215d8f9de3c071aa99ce4e0a
bbjorn <bbjorn@student.chalmers.se>
2020-07-22 10:42:15 +0200

    Did tutorial on Fetching Data with Hooks.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import logo from "./logo.svg";
 import "./App.css";
 import Movie from "./hoc/Movie";
 import Counter from "./hooks/Counter";
+import Users from "./hooks/Users";
 
 function App() {
-  return <Counter />;
+  return <Users />;
 }
 
 export default App;
