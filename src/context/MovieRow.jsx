01fc696206fc49e9f9eb19918d415a2b19600173
bbjorn <bbjorn@student.chalmers.se>
2020-07-22 11:31:22 +0200

    Did tutorial on consuming multiple contexts

--- src/context/MovieRow.jsx
+++ src/context/MovieRow.jsx
@@ -1,9 +1,13 @@
 import React, { useContext } from "react";
 import UserContext from "./userContext";
+import CartContext from "./cartContext";
 
 function MovieRow(props) {
   const userContext = useContext(UserContext);
-  console.log("context", userContext);
+  console.log("User context", userContext);
+
+  const cartContext = useContext(CartContext);
+  console.log("Cart context", cartContext);
 
   return (
     <div>
